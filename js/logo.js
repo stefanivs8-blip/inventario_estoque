3baaa4aa4b35d5a212ac99973143ce0b4da481f9
stefanivs8-blip <stefanivs8@gmail.com>
2026-05-11 01:30:42 -0300

    Update logo.js

--- js/logo.js
+++ js/logo.js
@@ -1,3 +1,1 @@
-<script>
-  const PORTOCEL_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Logotipo_Portocel.png/800px-Logotipo_Portocel.png";
-</script>
+const PORTOCEL_LOGO = "https://logodownload.org/wp-content/uploads/2021/04/portocel-logo.png";
